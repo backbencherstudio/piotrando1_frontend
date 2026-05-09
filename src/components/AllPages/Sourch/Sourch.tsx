f24ce7b66b221d0c2fc489f1f31255200efb8d57
SujonRoyBD <sujonroy537527@gmail.com>
2026-05-09 18:42:33 +0600

    dd

--- src/components/AllPages/Sourch/Sourch.tsx
+++ src/components/AllPages/Sourch/Sourch.tsx
@@ -89,7 +89,7 @@ export default function Sourch() {
 
                 <div className='  ' data-aos="fade-left"
                     data-aos-duration="2000">
-                    <Image src="/images/sourch2.png" alt='image' height={501} width={545} className='z-10 h-[271px] lg:h-[501px] w-full rounded-xl' />
+                    <Image src="/images/image33.png" alt='image' height={501} width={545} className='z-10 h-[271px] lg:h-[501px] w-full rounded-xl' />
 
                 </div>
 
