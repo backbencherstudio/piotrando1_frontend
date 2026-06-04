d8f208991ccc86eea6c6b6fa946116042eddab04
SujonRoyBD <sujonroy537527@gmail.com>
2026-06-04 15:09:53 +0600

    next

--- src/components/AllPages/OurProduct/EvoluationPack.tsx
+++ src/components/AllPages/OurProduct/EvoluationPack.tsx
@@ -36,7 +36,7 @@ export default function Evaluation() {
 
                                 {/* TEXT */}
                                 <div>
-                                    <div className="max-w-[58%] sm:max-w-[55%] mt-9.5">
+                                    <div className="max-w-[58%] sm:max-w-[55%] mt-4 md:mt-9.5">
                                         <h2 className="flex items-center gap-2 text-lg font-semibold sm:text-2xl md:text-3xl">
                                             <span className="h-2 w-2 rounded-full bg-[#D5EB2A]"></span>
                                             <p className=" text-[#101828] ">{t.products.evolution.insidePacks.title}</p>
@@ -105,7 +105,7 @@ export default function Evaluation() {
                                 {/* IMAGE */}
                                 <div
                                     onClick={() => setIsFlipped(!isFlipped)}
-                                    className="group perspective absolute top-1/3 md:top-1/2 mt-12 md:mt-3 right-[2px] sm:right-[25px] md:right-[20px] lg:right-[-50px] xl:right-[10px] 2xl:right-[14px] flex -translate-y-1/2 cursor-pointer items-center justify-center rounded-xl "
+                                    className="group perspective absolute top-1/3 md:top-1/2 mt-10 md:mt-3 right-[2px] sm:right-[25px] md:right-[20px] lg:right-[-50px] xl:right-[10px] 2xl:right-[14px] flex -translate-y-1/2 cursor-pointer items-center justify-center rounded-xl "
                                 >
                                     <div
                                         className={`relative h-[220px] w-[150px] sm:h-[280px] sm:w-[190px] md:h-[320px] md:w-[220px] transition-transform duration-700 transform-style-preserve-3d rounded-xl ${isFlipped ? "rotate-y-180" : "group-hover:rotate-y-180"
