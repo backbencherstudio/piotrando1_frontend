d6535db7d6c2f77d599392685027083292a043f6
SujonRoyBD <sujonroy537527@gmail.com>
2026-06-22 14:02:51 +0600

    update

--- src/components/AllPages/OurProduct/UltraPack.tsx
+++ src/components/AllPages/OurProduct/UltraPack.tsx
@@ -39,7 +39,7 @@ export default function UltraPack() {
                             {/* TEXT */}
                             <div>
                                 <div className="max-w-[58%] sm:max-w-[56%] max-[363px]:w-[132px] max-[330px]:max-w-[120px] space-y-3 text-sm text-[#4A5565] sm:text-base">
-                                    <h2 className="flex gap-1 sm:gap-2 font-semibold max-[366px]:text-[15px] text-lg sm:text-2xl md:text-3xl">
+                                    <h2 className="flex max-[366px]:gap-1 gap-2 font-semibold max-[366px]:text-[15px] text-lg sm:text-2xl md:text-3xl">
                                         <span className="block rounded-full  h-2 w-2 max-[350px]:h-1.5 max-[350px]:w-1.5 mt-2 md:mt-3 bg-[#FF5286]"></span>
 
 
